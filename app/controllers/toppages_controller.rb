0947d2715df651cf5741812e69bcad02e969e441
akinori.matsumoto <wbraver@outlook.jp>
2021-01-28 22:15:18 +0900

    toppage view revised

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,6 +3,8 @@ class ToppagesController < ApplicationController
     if logged_in?
       @paper = current_user.papers.build #form_with用
       @papers = current_user.papers.order(id: :desc).page(params[:page]).per(10)
+    else
+      @user = User.new
     end
   end
 end
